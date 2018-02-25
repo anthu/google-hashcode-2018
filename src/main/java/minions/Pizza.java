04b4bdac0ddb9d40f31839b9627aad18579da82d
David Eibl <david.eibl@maibornwolff.de>
2018-02-25 16:38:27 +0100

    .

--- src/main/java/minions/Pizza.java
+++ src/main/java/minions/Pizza.java
@@ -5,12 +5,27 @@ import java.util.List;
 
 public class Pizza {
     private final List<List<Cell>> rowsWholePizza;
+
+    private final List<List<Cell>> rowsRemainingPizza;
+
     private final List<Slice> slices;
 
     public Pizza(List<List<Cell>> rowsWholePizza) {
         this.rowsWholePizza = rowsWholePizza;
+
+        rowsRemainingPizza = new ArrayList<>();
+        for(List<Cell> row : rowsWholePizza) {
+            List<Cell> rowCopy = new ArrayList<>();
+            rowCopy.addAll(row);
+            rowsRemainingPizza.add(rowCopy);
+        }
+
         slices = new ArrayList<>();
     }
 
+    public void addSlice(Slice slice) {
+        slices.add(slice);
+
+    }
 
 }
