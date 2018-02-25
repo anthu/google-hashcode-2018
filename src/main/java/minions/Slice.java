b8f95a9999d08e87cb0648cc30ad11cb7edcb56d
Anton Huck <antonhuck@gmail.com>
2018-02-25 16:49:28 +0100

    Change slice cooordinate not to be final

--- src/main/java/minions/Slice.java
+++ src/main/java/minions/Slice.java
@@ -1,13 +1,11 @@
 package minions;
 
 public class Slice {
-    private final Coordinate coordinate1;
-    private final Coordinate coordinate2;
+    private Coordinate coordinate1;
+    private Coordinate coordinate2;
 
     public Slice(Coordinate coordinate1, Coordinate coordinate2) {
         this.coordinate1 = coordinate1;
         this.coordinate2 = coordinate2;
     }
-
-
 }
