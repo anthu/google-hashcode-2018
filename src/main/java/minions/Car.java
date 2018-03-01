573f3ba9672cf04bcff4036ac957ffe423355a50
Anton Huck <antonhuck@gmail.com>
2018-03-01 19:52:02 +0100

    la

--- src/main/java/minions/Car.java
+++ src/main/java/minions/Car.java
@@ -10,7 +10,8 @@ public class Car {
     private int timeAfterLastRide = 0;
     private int columnPosition = 0;
     private int rowPosition = 0;
-
+    private int currentTime = 0;
+    private int currentScore = 0;
     public void addRide(Ride ride){
         int timeToStart = Math.abs(columnPosition - ride.getColumnStart()) + Math.abs(rowPosition - ride.getRowStart());
         int timeAtStartPositionArrived = timeAfterLastRide + timeToStart;
@@ -35,5 +36,35 @@ public class Car {
 
     public int getRowPosition() {
         return rowPosition;
+    }
+
+    public int score(int b) {
+        currentTime = 0;
+        currentScore = 0;
+        Ride previousRide = new Ride(0,0,0,0,-1,-1, -1);
+        for (Ride ride : this.rides) {
+            if (second(ride)) {
+                currentScore += b;
+            }
+            if(firstTrue(previousRide,ride)) {
+                currentScore += ride.getDuration();
+            }
+        }
+        return currentScore;
+    }
+
+    private boolean firstTrue(Ride previous, Ride current) {
+        int arTime = getArrival(previous, current);
+        int start = (arTime < current.getEarliestStart()) ? arTime : this.currentTime;
+        this.currentTime = (start + current.getDuration());
+        return this.currentTime < current.getLatestFinish();
+    }
+
+    private static int getArrival(Ride prev, Ride cur) {
+        return Math.abs(prev.getRowFinish() - cur.getRowStart()) + Math.abs(prev.getColumnFinish() - cur.getRowStart());
+    }
+
+    private boolean second(Ride current) {
+        return (current.getEarliestStart() == this.currentTime);
     }
 }
