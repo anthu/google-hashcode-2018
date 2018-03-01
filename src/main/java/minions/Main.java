b7e0af1adb67a84eeeee86fc21dcaadfedfe3148
David Eibl <david.eibl@maibornwolff.de>
2018-03-01 18:26:17 +0100

    remove

--- src/main/java/minions/Main.java
+++ src/main/java/minions/Main.java
@@ -6,14 +6,10 @@ public class Main {
     public static void main(String ... args) {
         FileReader fileReader = new FileReader("medium.in");
 
-        Pizza pizza = fileReader.getPizza();
         int columnCount = fileReader.getColumnCount();
         int rowCount = fileReader.getRowCount();
         int minimumIngredients = fileReader.getMinimumIngredientsCount();
         int maxSize = fileReader.getMaxSize();
 
-        System.out.println("Read pizza config was:");
-        System.out.println("rows: " + rowCount + ", columns: " + columnCount + ", minimum ingredients:" +
-                minimumIngredients + ", max size: " + maxSize);
     }
 }
