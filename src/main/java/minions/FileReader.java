18c871802787932aae79ee2e268b0f6876ab5a55
Anton Huck <antonhuck@gmail.com>
2018-03-03 23:48:10 +0100

    Refactoring

--- src/main/java/minions/FileReader.java
+++ src/main/java/minions/FileReader.java
@@ -1,11 +1,7 @@
 package minions;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -19,48 +15,45 @@ public class FileReader {
     private int ridesCount;
     private int bonusCount;
     private int numberOfSteps;
+
     FileReader(String path) {
-        try {
-            try (BufferedReader br = new BufferedReader(new java.io.FileReader(path))) {
-                try {
-                    String line = br.readLine();
-                    String[] lineSplit = line.split(" ");
-                    rows = Integer.valueOf(lineSplit[0]);
-                    columns = Integer.valueOf(lineSplit[1]);
-                    vehiclesCount = Integer.valueOf(lineSplit[2]);
-                    ridesCount = Integer.valueOf(lineSplit[3]);
-                    bonusCount = Integer.valueOf(lineSplit[4]);
-                    numberOfSteps = Integer.valueOf(lineSplit[5]);
+        File f = new File(path);
+        try (
+            BufferedReader br = new BufferedReader(new java.io.FileReader(f))
+        ) {
+            String line = br.readLine();
+            String[] lineSplit = line.split(" ");
+            rows = Integer.valueOf(lineSplit[0]);
+            columns = Integer.valueOf(lineSplit[1]);
+            vehiclesCount = Integer.valueOf(lineSplit[2]);
+            ridesCount = Integer.valueOf(lineSplit[3]);
+            bonusCount = Integer.valueOf(lineSplit[4]);
+            numberOfSteps = Integer.valueOf(lineSplit[5]);
 
 
-                    int id = 0;
-                    int skipped = 0;
+            int id = 0;
+            int skipped = 0;
 
-                    while ((line = br.readLine()) != null) {
-                        lineSplit = line.split(" ");
+            while ((line = br.readLine()) != null) {
+                lineSplit = line.split(" ");
 
-                        int rowStart = Integer.valueOf(lineSplit[0]);
-                        int columnStart = Integer.valueOf(lineSplit[1]);
-                        int rowFinish = Integer.valueOf(lineSplit[2]);
-                        int columnFinish = Integer.valueOf(lineSplit[3]);
-                        int earliestStart = Integer.valueOf(lineSplit[4]);
-                        int latestFinish = Integer.valueOf(lineSplit[5]);
+                int rowStart = Integer.parseInt(lineSplit[0]);
+                int columnStart = Integer.parseInt(lineSplit[1]);
+                int rowFinish = Integer.parseInt(lineSplit[2]);
+                int columnFinish = Integer.parseInt(lineSplit[3]);
+                int earliestStart = Integer.parseInt(lineSplit[4]);
+                int latestFinish = Integer.parseInt(lineSplit[5]);
 
-                        Ride ride = new Ride(id++, rowStart, columnStart, rowFinish, columnFinish, earliestStart, latestFinish);
+                Ride ride = new Ride(id++, rowStart, columnStart, rowFinish, columnFinish, earliestStart, latestFinish);
 
-                        int diff = ride.getLatestFinish()-ride.getEarliestStart();
-                        if(ride.getDuration() <= diff) {
-                            rides.add(ride);
-                        } else {
-                            skipped++;
-                        }
-
-
-                    }
-
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                int diff = ride.getLatestFinish()-ride.getEarliestStart();
+                if(ride.getDuration() <= diff) {
+                    rides.add(ride);
+                } else {
+                    skipped++;
                 }
+
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -68,7 +61,9 @@ public class FileReader {
 
         List<Car> bestCars = getNextSortedSkipLate(rides);
 
-        try(PrintWriter br = new PrintWriter(new FileWriter("output/" + path.replace(".in", ".out")))) {
+        try(
+                PrintWriter br = new PrintWriter(new FileWriter("output/" + f.getName().replace(".in", ".out")))
+        ) {
             for(Car car: bestCars) {
                 br.write(car.toString() + "\n");
             }
@@ -119,8 +114,6 @@ public class FileReader {
         for (int j = 0; j < vehiclesCount ; j++) {
             bestCars.add(new Car());
         }
-        int removed = 0;
-        int tmp = 0;
 
         for(Ride ride : rides) {
             Car avcar = bestCars.get(0);
@@ -132,10 +125,6 @@ public class FileReader {
             if ((getTimeTocome(avcar,ride) + ride.getDuration()) < ride.getLatestFinish())
             {
                 avcar.addRide(ride);
-                tmp = 0;
-            } else {
-                removed+=ride.getDuration();
-                tmp++;
             }
         }
 
@@ -176,7 +165,6 @@ public class FileReader {
                 removed++;
             }
         }
-        System.out.println("removed" + removed);
         return bestCars;
     }
 
--- src/main/java/minions/Main.java
+++ src/main/java/minions/Main.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String ... args) {
-        new FileReader("a_example.in");
-        new FileReader("b_should_be_easy.in");
-        new FileReader("c_no_hurry.in");
-        new FileReader("d_metropolis.in");
-        new FileReader("e_high_bonus.in");
+        new FileReader("input/a_example.in");
+        new FileReader("input/b_should_be_easy.in");
+        new FileReader("input/c_no_hurry.in");
+        new FileReader("input/d_metropolis.in");
+        new FileReader("input/e_high_bonus.in");
     }
 }
